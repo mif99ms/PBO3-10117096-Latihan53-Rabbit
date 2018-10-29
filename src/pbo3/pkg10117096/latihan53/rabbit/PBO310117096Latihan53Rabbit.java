5a79e60c5f548ea7ba351f35767100ea43e0b50e
mif99ms <msidqi86@gmail.com>
2018-10-29 13:25:21 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan53/rabbit/PBO310117096Latihan53Rabbit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan53.rabbit;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ * Deskripsi    : Program ini berisi program untuk menampilkan nama binatang
+ */
+public class PBO310117096Latihan53Rabbit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+    }
+    
+}
---
+++ src/pbo3/pkg10117096/latihan53/rabbit/Rabbit.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan53.rabbit;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ */
+public class Rabbit extends Animal {
+    private String color;
+    private String name;
+
+    public Rabbit(String name, boolean veg, String food, int legs, String color) {
+        super(veg,food,legs); //lakukan hal tambahan -> parameter yg berasal dari superClass
+        this.name = name;
+        this.vegetarian = veg;
+        this.eats = food;
+        this.noOfLegs = legs;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    } 
+}
